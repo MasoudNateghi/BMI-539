865d9419edda137eaccfd13c29f8611f43d0005e
MasoudNateghi <masoudnateghi78@gmail.com>
2024-12-01 22:58:14 -0500

    modified utils

--- project/codes/utils/processing.py
+++ project/codes/utils/processing.py
@@ -3,25 +3,25 @@ from utils.lpf import lp_filter_zero_phase
 from scipy.signal import butter, sosfilt, resample, iirnotch, tf2sos
 
 
-def preproc(signal, fc, fs_new, fs, order=64, Q_factor=30, freq=50):
+def preproc(signal, fc, fs, fs_old, order=64, Q_factor=30, freq=50):
     # Design the filter
-    normalized_cutoff = fs_new / fs
+    normalized_cutoff = fs / fs_old
     sos = butter(order, normalized_cutoff, btype='low', output='sos')
 
     # Design the notch filter
-    w0 = freq / (fs_new / 2)  # Normalize frequency to Nyquist
+    w0 = freq / (fs / 2)  # Normalize frequency to Nyquist
     b, a = iirnotch(w0, Q_factor)
     sos_notch = tf2sos(b, a)
 
     # Prepare to store the downsampled signal
-    num_samples_new = int(signal.shape[1] * fs_new / fs)  # New time dimension
+    num_samples_new = int(signal.shape[1] * fs / fs_old)  # New time dimension
     preprocessed_signal = np.zeros((signal.shape[0], num_samples_new))
 
     # Process each channel independently
     nChannels = signal.shape[0]
     for ch in range(nChannels):
         # Apply low-pass filter
-        lpf_signal = signal[ch] - lp_filter_zero_phase(signal[ch], fc/fs)
+        lpf_signal = signal[ch] - lp_filter_zero_phase(signal[ch], fc/fs_old)
         # Apply the antialiasing filter
         filtered_signal = sosfilt(sos, lpf_signal)
         # Resample
--- project/codes/utils/variables.py
+++ project/codes/utils/variables.py
@@ -3,20 +3,19 @@
 import wfdb
 from utils.lpf import lp_filter_zero_phase
 
-from project.codes.demo_load_data import fs_new
-
 # variables
-fs = 1000  # Sampling frequency
+fs_old = 1000  # Sampling frequency
 fc = 0.5  # Cut-off frequency
-fs_new = 360  # New sampling frequency
+fs = 360  # New sampling frequency
 
 # paths
-run_local = True
+run_local = False
 if run_local:
     dataset_path = "/Users/masoud/Documents/Education/Alphanumerics Lab/Projects/data/physionet.org/files/ptbdb/1.0.0"
     bw_path = "/Users/masoud/Documents/Education/Alphanumerics Lab/Projects/data/physionet.org/files/nstdb/1.0.0/bw"
 else:
-    pass
+    dataset_path = "/labs/samenilab/team/masoud_nateghi/data/physionet.org/files/ptbdb/1.0.0/"
+    bw_path = "/labs/samenilab/team/masoud_nateghi/data/physionet.org/files/nstdb/1.0.0/bw"
 
-bw = wfdb.rdrecord(bw_path).p_signal.T[0]
-bw = lp_filter_zero_phase(bw, fc / fs_new)
+bw = wfdb.rdrecord(bw_path).p_signal.T[0]  # read bw
+bw = lp_filter_zero_phase(bw, fc / fs)  # remove high frquency noise from bw
